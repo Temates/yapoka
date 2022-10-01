2d95e93ecaaaab78149be05c548a78938a912f3d
Temates <c11190016@john.petra.ac.id>
2022-10-01 23:12:15 +0700

    -Search Button dan Pagination -Search berdasar kategori -hapus categories dan author pada \web.php

--- routes/web.php
+++ routes/web.php
@@ -41,26 +41,11 @@ Route::get('/posts', [PostController::class, 'index']);
 
 //halaman single post
 Route::get('posts/{post:slug}',[PostController::class, 'show']);
+
 Route::get('/categories', function(Category $category){
     return view('categories',[
         'title' => 'Post Categories',
         'active' => 'categories',
         'categories'=> Category::all()
-    ]);
-});
-Route::get('/categories/{category:slug}', function(Category $category){
-    return view('posts',[
-        'title' => "Post by Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category','author'),
-        
-    ]);
-});
-Route::get('/authors/{author:username}', function(User $author){
-    return view('posts',[
-        'title' => "Post by Author : $author->name",
-        
-        'posts' => $author->posts->load('category','author')
-        
     ]);
 });
